98061b9404cd3b8e1446ed1e8733fecf17e5a09d
Penelope Drago <prd@drago.com.au>
2023-03-22 20:10:08 +1100

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+// Function to find matching drivers
+
+function findMatching(driverList, driverName) {
+  const driverMatch = driverList.filter(item => item.toLowerCase() === driverName.toLowerCase());
+  return driverMatch;
+}
+
+// Function to find drivers with beginning matching letters
+
+function fuzzyMatch(driverList, driverName) {
+  const driverMatch = driverList.filter(item => item.startsWith(driverName));
+  return driverMatch;
+}
+
+// Function to match name value
+
+function matchName(driverList, driverName) {
+  const driverMatch = driverList.filter(item => item.name === driverName);
+  return driverMatch;
+}
